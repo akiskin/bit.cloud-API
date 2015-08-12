b0a6fa88f876a71d7a02e8113e98631ca096e6d7
AKiskin <estel4ever@gmail.com>
2015-08-12 12:09:05 +0300

    First version

---
+++ api_urls.js
@@ -0,0 +1,7 @@
+function publicapiurl() {
+	return "https://fresh03.bit-live.ru/system/api/v01/hs/corepublicapi"
+}
+
+function privateapiurl() {
+	return "https://fresh03.bit-live.ru/system/privateapi/v01/hs/coreprivateapi"
+}
